30816b47540ec19951757791e45c0f2b6d3aabae
codewithlaibashahid <malikshazy32@gmail.com>
2025-01-26 03:35:01 -0800

    next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -12,7 +12,7 @@ const nextConfig = {
 };
 module.exports = {
   experimental: {
-    runtime: 'nodejs',  // Force using Node.js instead of Edge
+     "type": "module"
   },
 };
 export default nextConfig;
